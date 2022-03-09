4ee9b43f61314bc5026428cc51aa315c24c34e40
heqi <moeei@qq.com>
2022-03-09 20:43:02 +0800

    讲师逻辑删除接口

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -4,10 +4,7 @@ package com.atguigu.eduservice.controller;
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +28,11 @@ public class EduTeacherController {
         List<EduTeacher> list = eduTeacherService.list(null);
         return list;
     }
-    
+
+    @DeleteMapping("{id}")
+    public boolean removeTeacher(@PathVariable String id){
+        boolean flag = eduTeacherService.removeById(id);
+        return flag;
+    }
 }
 
