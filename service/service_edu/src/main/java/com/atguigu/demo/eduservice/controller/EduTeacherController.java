a7d3b760b49f8e321cc5e502e1f1ffaa4f54915e
heqi <moeei@qq.com>
2022-03-09 17:44:10 +0800

    项目环境搭建

---
+++ service/service_edu/src/main/java/com/atguigu/demo/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.demo.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author heqi
+ * @since 2022-03-09
+ */
+@RestController
+@RequestMapping("/eduservice/teacher")
+public class EduTeacherController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/atguigu/demo/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.demo.eduservice.mapper;
+
+import com.atguigu.demo.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author heqi
+ * @since 2022-03-09
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
