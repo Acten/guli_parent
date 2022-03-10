af89aae54148af78a2167fa8d5c4a5f6d3b04b61
heqi <moeei@qq.com>
2022-03-10 11:08:33 +0800

    分页查询讲师/条件查询讲师带分页

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,11 +3,15 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -44,6 +48,46 @@ public class EduTeacherController {
         }else {
             return R.error();
         }
+    }
+
+    @ApiOperation(value = "分页查询讲师")
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@ApiParam(name = "current",value = "当前页",required = true)@PathVariable long current,
+                             @ApiParam(name = "limit",value = "每页记录数",required = true)@PathVariable long limit){
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        eduTeacherService.page(pageTeacher,null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total",total).data("rows",records);
+    }
+
+    @ApiOperation(value = "条件查询讲师带分页")
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@ApiParam(name = "current",value = "当前页",required = true)@PathVariable Long current,
+                                  @ApiParam(name = "limit",value = "每页记录数",required = true)@PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery){
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        if(!StringUtils.isEmpty(name)){
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)){
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)){
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)){
+            wrapper.le("gmt_create",end);
+        }
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        eduTeacherService.page(pageTeacher,wrapper);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total",total).data("rows",records);
     }
 }
 
