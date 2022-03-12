526ec4ad98e20979b8a4d8751fe2f990663adfe9
heqi <moeei@qq.com>
2022-03-12 16:28:19 +0800

    fix bugs

--- service/service_oss/src/main/java/com/atguigu/oss/controller/OssController.java
+++ service/service_oss/src/main/java/com/atguigu/oss/controller/OssController.java
@@ -2,13 +2,14 @@ package com.atguigu.oss.controller;
 
 import com.atguigu.commonutils.R;
 import com.atguigu.oss.service.OssService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Api(description="阿里云文件管理")
 @RestController
 @RequestMapping("eduservice/fileoss")
 @CrossOrigin
@@ -17,8 +18,10 @@ public class OssController {
     @Autowired
     private OssService ossService;
 
+    @ApiOperation(value = "文件上传")
     @PostMapping
-    public R uploadOssFile(MultipartFile file){
+    public R uploadOssFile(@ApiParam(name = "file", value = "文件", required = true)
+                           @RequestParam("file") MultipartFile file){
         String url = ossService.uploadFileAvatar(file);
         return R.ok().data("url",url);
     }
