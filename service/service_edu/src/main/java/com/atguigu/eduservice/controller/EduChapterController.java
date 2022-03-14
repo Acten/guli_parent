18a43e9cd4c47b87134fcceee0116ad61bc7d522
heqi <moeei@qq.com>
2022-03-14 16:34:31 +0800

    添加课程信息接口

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
@@ -0,0 +1,26 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.commonutils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程 前端控制器
+ * </p>
+ *
+ * @author heqi
+ * @since 2022-03-14
+ */
+@RestController
+@RequestMapping("/eduservice/chapter")
+@CrossOrigin
+public class EduChapterController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
@@ -0,0 +1,35 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.atguigu.eduservice.service.EduCourseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 课程 前端控制器
+ * </p>
+ *
+ * @author heqi
+ * @since 2022-03-14
+ */
+@Api(description = "课程信息管理")
+@RestController
+@RequestMapping("/eduservice/course")
+@CrossOrigin
+public class EduCourseController {
+    @Autowired
+    private EduCourseService courseService;
+
+    @ApiOperation("添加课程基本信息和简介信息")
+    @PostMapping("addCourseInfo")
+    public R addCourseInfo(@RequestBody CourseInfoVo courseInfoVo){
+        courseService.saveCourseInfo(courseInfoVo);
+        return R.ok();
+    }
+}
+
