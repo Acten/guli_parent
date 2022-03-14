2e79b07ea1cc3a9624111bdca225cb57fcae6691
heqi <moeei@qq.com>
2022-03-14 19:11:55 +0800

    细节问题处理

--- service/service_edu/src/main/java/com/atguigu/eduservice/entity/vo/CourseInfoVo.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/entity/vo/CourseInfoVo.java
@@ -16,7 +16,7 @@ public class CourseInfoVo {
     @ApiModelProperty(value = "课程专业ID")
     private String subjectId;
 
-    @ApiModelProperty(value = "课程专业父级ID")
+    @ApiModelProperty(value = "一级分类ID")
     private String subjectParentId;
 
     @ApiModelProperty(value = "课程标题")
