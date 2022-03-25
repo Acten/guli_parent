b18f3bcb001dcd2a1683faefabe88ddc6cc98173
heqi <moeei@qq.com>
2022-03-25 11:01:40 +0800

    微信扫码支付

--- service/service_order/src/main/java/com/atguigu/eduorder/controller/OrderController.java
+++ service/service_order/src/main/java/com/atguigu/eduorder/controller/OrderController.java
@@ -3,7 +3,9 @@ package com.atguigu.eduorder.controller;
 
 import com.atguigu.commonutils.JwtUtils;
 import com.atguigu.commonutils.R;
+import com.atguigu.eduorder.entity.Order;
 import com.atguigu.eduorder.service.OrderService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,10 +27,19 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    @PostMapping("createOrder/{couseId}")
+    @PostMapping("createOrder/{courseId}")
     public R saveOrder(@PathVariable String courseId, HttpServletRequest request){
         String orderNo = orderService.createOrders(courseId, JwtUtils.getMemberIdByJwtToken(request));
         return R.ok().data("orderId",orderNo);
+    }
+
+    //2 根据订单id查询订单信息
+    @GetMapping("getOrderInfo/{orderId}")
+    public R getOrderInfo(@PathVariable String orderId) {
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        wrapper.eq("order_no",orderId);
+        Order order = orderService.getOne(wrapper);
+        return R.ok().data("item",order);
     }
 }
 
